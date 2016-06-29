f0f1ef821118f1210544eb0813323cfb258a496b
unknown <>
2016-06-29 10:59:55 +0800

    0001

---
+++ lattern_web/lattern/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.URLField(blank=True)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
