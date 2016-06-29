e7aa5aa90eb2d0b155f8d18394bc8b2efe596b18
YuanSyun <rty3497374@gmail.com>
2016-06-29 18:02:13 +0800

    APP ITD Page

---
+++ 2016_Hackathon_20/app/src/main/java/yuansyun/a2016_hackathon_20/Introduction.java
@@ -0,0 +1,19 @@
+package yuansyun.a2016_hackathon_20;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
--- 2016_Hackathon_20/app/src/main/java/yuansyun/a2016_hackathon_20/MainActivity.java
+++ 2016_Hackathon_20/app/src/main/java/yuansyun/a2016_hackathon_20/MainActivity.java
@@ -1,7 +1,10 @@
 package yuansyun.a2016_hackathon_20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextPageBtn = (Button)findViewById(R.id.ITDBtn);
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this , Introduction.class);
+                startActivity(intent);
+            }
+        });
     }
 }
